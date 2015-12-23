8754953238e9d230be7ab9d0b4c5d41ca1efea7a
Mary Grace Lucas <graceylucas@hotmail.com>
2015-12-22 22:54:28 -0500

    Sets up location manager

--- FaveLocations/ViewController.swift
+++ FaveLocations/ViewController.swift
@@ -9,18 +9,65 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    
+    var manager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+            self.manager = CLLocationManager()
+            self.manager.delegate = self
+            self.manager.desiredAccuracy = kCLLocationAccuracyBest
+            self.manager.requestWhenInUseAuthorization()
+            self.manager.startUpdatingLocation()
+        
+    
+        
     }
 
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        print(locations)
+        
+        let userLocation: CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta:CLLocationDegrees = 0.02
+        
+        let longDelta:CLLocationDegrees = 0.02
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
+       
+        
+        
     }
 
 
